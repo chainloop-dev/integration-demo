dafcdb46acadf82cd7634d5377c63476fbdc87c6
Miguel Martinez Trivino <migmartri@gmail.com>
2024-03-06 17:57:15 +0100

    Revert "chore: remove Chainloop to reset demo"
    This reverts commit bf3ae12f6449bade5677f63700c370821111ae2e.

--- chainloop-demo/dagger/src/main.go
+++ chainloop-demo/dagger/src/main.go
@@ -11,8 +11,28 @@ type BuildAndRelease struct{}
 
 // - Build, package and publish a Go application as a container image
 // - Generate a CycloneDX Software Bill Of Materials using Syft
-func (m *BuildAndRelease) BuildAndPublish(ctx context.Context, proj *Directory) (string, error) {
+// - Attest the pieces of evidence (binary, container image, and SBOM) using Chainloop
+// https://docs.chainloop.dev/getting-started/attestation-crafting
+func (m *BuildAndRelease) BuildAndPublish(ctx context.Context, proj *Directory, chainloopToken, chainloopSigningKey, chainloopPassphrase *Secret) (string, error) {
 	var err error
+	chainloopClient := dag.Chainloop(chainloopToken)
+
+	// Initialize the attestation
+	attestationID, err := chainloopClient.AttestationInit(ctx, ChainloopAttestationInitOpts{Repository: proj})
+	if err != nil {
+		return "", fmt.Errorf("failed to initialize attestation: %w", err)
+	}
+
+	// Finish/Mark as failed the attestation once we are done
+	defer func() {
+		// If there was an error in the process, mark the attestation as failed in Chainloop
+		if err != nil {
+			chainloopClient.AttestationReset(ctx, attestationID, ChainloopAttestationResetOpts{Reason: err.Error()})
+		} else {
+			// Push the attestation to Chainloop
+			_, err = chainloopClient.AttestationPush(ctx, attestationID, chainloopSigningKey, chainloopPassphrase)
+		}
+	}()
 
 	// Build software artifacts
 	res, err := m.doBuildAndPublish(ctx, proj)
@@ -20,7 +40,27 @@ func (m *BuildAndRelease) BuildAndPublish(ctx context.Context, proj *Directory) 
 		return "", fmt.Errorf("failed to build and publish artifacts: %w", err)
 	}
 
-	return res.imageRepo, nil
+	// Attest the pieces of evidence
+	// Container image
+	_, err = chainloopClient.AttestationAdd(ctx, attestationID, "image", ChainloopAttestationAddOpts{Value: res.imageRepo})
+	if err != nil {
+		return "", fmt.Errorf("failed to add container image piece of evidence: %w", err)
+	}
+
+	// Binary
+	_, err = chainloopClient.AttestationAdd(ctx, attestationID, "binary", ChainloopAttestationAddOpts{Path: res.binary})
+	if err != nil {
+		return "", fmt.Errorf("failed to add binary piece of evidence: %w", err)
+	}
+
+	// SBOM
+	_, err = chainloopClient.AttestationAdd(ctx, attestationID, "sbom", ChainloopAttestationAddOpts{Path: res.sbom})
+	if err != nil {
+		return "", fmt.Errorf("failed to add SBOM piece of evidence: %w", err)
+	}
+
+	// Return information about the attestation
+	return chainloopClient.AttestationStatus(ctx, attestationID)
 }
 
 type buildResult struct {
